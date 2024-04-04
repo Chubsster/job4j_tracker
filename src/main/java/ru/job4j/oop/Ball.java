3cc045f1e90c7db25904c698fa83c6c0e256d9f7
Chubsster <evgenshama37@gmail.com>
2024-04-04 17:19:59 +0500

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+import java.sql.SQLOutput;
+
+public class Ball {
+    public void tryRun(boolean condition) {
+        if (condition) {
+            System.out.println("Колобок сьеден");
+        } else {
+            System.out.println("Колобок сбежал");
+        }
+    }
+}
