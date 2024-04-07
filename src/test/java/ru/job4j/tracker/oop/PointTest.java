87ed551e8e1d66bc1eb8024628c5f81cbef59986
Chubsster <evgenshama37@gmail.com>
2024-04-07 23:11:27 +0500

    2.0. Рефакторинг - Расстояние между точками. [#122587]

---
+++ src/test/java/ru/job4j/tracker/oop/PointTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.tracker.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static java.awt.geom.Point2D.distance;
+import static org.assertj.core.api.Assertions.*;
+
+public class PointTest {
+    @Test
+    public void when00To20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double output = distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    public void when22to42then2() {
+        double expected = 2;
+        int x1 = 2;
+        int y1 = 4;
+        int x2 = 2;
+        int y2 = 2;
+        double output = distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    public void whenMinus32toMinus62then9dot43() {
+        double expected = 9.43;
+        int x1 = -3;
+        int y1 = -6;
+        int x2 = 2;
+        int y2 = 2;
+        double output = distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    public void when5Minus1to64then6dot32() {
+        double expected = 6.32;
+        int x1 = 5;
+        int y1 = 6;
+        int x2 = -1;
+        int y2 = 4;
+        double output = distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+}
