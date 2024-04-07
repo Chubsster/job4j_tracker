5700969a33fe645b2a5da08225b51e3ea0b25007
Chubsster <evgenshama37@gmail.com>
2024-04-07 17:35:55 +0500

    6. Правила Checkstyle [#504981 #489760]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -3,16 +3,22 @@ package ru.job4j.checkstyle;
 public class Broken {
     private int sizeOfEmpty = 10;
     public String surname;
+
     void echo() { }
+
     void method(int a, int b, int c, int d) {
     }
+
     public static final String NEWVALUE = "";
+
     String name;
+
     void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
+
     Broken() {
     }
 }
