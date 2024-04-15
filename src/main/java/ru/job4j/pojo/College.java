28c16ec72cdd1ed21b353225cf70e1ab6642a3c1
Chubsster <evgenshama37@gmail.com>
2024-04-15 22:25:13 +0500

    0. Что такое инкапсуляция. [#3818]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Shamaev Evgeny");
+        student.setGroup("Java");
+        student.setCreated(new Date());
+        ;
+
+        System.out.println(student.getFullName() + " joins the group " + student.getGroup() + " when arrived " + student.getCreated());
+    }
+}
